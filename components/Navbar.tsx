a2840276295e1477c7f9c9e115c6c9a60e774e0a
mkit2009 <matejkotrba.skola@gmail.com>
2022-12-23 11:27:47 +0100

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full px-12 py-6 text-2xl text-white bg-transparent border-b-2 border-white border-solid backdrop-blur-md">
+      <div className="container flex gap-5 mx-auto">
+        <p className="font-roboto">MyBlog</p>
+        {/* Other links */}
+
+        <Link
+          href={"/"}
+          className="relative ml-auto before:content-[''] before:absolute before:left-[50%] before:top-full
+        before:translate-x-[-50%] before:h-1 before:w-4 before:bg-white before:rounded-md hover:before:w-[105%]
+        before:duration-150 before:ease-in-out"
+        >
+          Home
+        </Link>
+        <Link
+          href={"/"}
+          className="relative before:content-[''] before:absolute before:left-[50%] before:top-full
+        before:translate-x-[-50%] before:h-1 before:w-4 before:bg-white before:rounded-md hover:before:w-[105%]
+        before:duration-150 before:ease-in-out"
+        >
+          Articles
+        </Link>
+        <Link
+          href={"/"}
+          className="relative before:content-[''] before:absolute before:left-[50%] before:top-full
+        before:translate-x-[-50%] before:h-1 before:w-4 before:bg-white before:rounded-md hover:before:w-[105%]
+        before:duration-150 before:ease-in-out"
+        >
+          About
+        </Link>
+        <Link
+          href={"/"}
+          className="relative before:content-[''] before:absolute before:left-[50%] before:top-full
+        before:translate-x-[-50%] before:h-1 before:w-4 before:bg-white before:rounded-md hover:before:w-[105%]
+        before:duration-150 before:ease-in-out"
+        >
+          Other
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Navbar } from "../components";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className={`${roboto.variable} ${roboto.className}`}>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
